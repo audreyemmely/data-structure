ac52f8932eff115d18dcb2654a00f776caa39596
Natália Assis <42848454+assisnat@users.noreply.github.com>
2020-10-27 21:55:29 -0300

    Add files via upload
    Pilha (quase completa)

---
+++ estruturas/stack.py
@@ -0,0 +1,47 @@
+class Elemento: 
+  def __init__(self, c, p):
+    self.conteudo = c
+    self.proximo = p
+
+
+class Pilha:
+  def __init__(self):
+    self.topo = None # pilha inicia vazia
+  
+  def vazia(self): # não recebe nada, apenas informa se a pilha está vazia ou não
+    if self.topo == None:
+      print ("A pilha está vazia\n")
+    else:
+      print ("A pilha não está vazia\n")
+
+  def empilhar(self, novo_conteudo): # adiciona um conteúdo na pilha
+    novo_elemento = Elemento(novo_conteudo, self.topo)
+    self.topo = novo_elemento
+    print("Elemento {} foi inserido no topo da pilha\n".format(novo_elemento.conteudo))
+
+  def desempilhar(self):
+    if self.topo == None:
+      print("A pilha está vazia\n")
+    elemento_excluido = self.topo
+    self.topo = self.topo.proximo
+    exclusao_feita = elemento_excluido.conteudo
+    del elemento_excluido
+    print("Elemento ({}) foi excluído do topo da pilha\n".format(exclusao_feita))
+
+  def tamanho(self):
+    if (self.topo == None):
+      return 0
+    contador_pilha = 0
+    conteudo_pilha = self.topo
+    while conteudo_pilha != None:
+      contador_pilha += 1
+      conteudo_pilha = conteudo_pilha.proximo
+    return contador_pilha
+
+  def listar(self):
+    if self.topo == None:
+      print("A pilha está vazia\n")
+    ver_conteudo = self.topo
+    while ver_conteudo != None:
+      print(ver_conteudo.numero)
+      ver_conteudo = ver_conteudo.proximo
