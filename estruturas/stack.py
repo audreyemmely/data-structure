5de6dfebbcd7410adb8a9c3f2131d5ba2c69f2b7
Natália Assis <42848454+assisnat@users.noreply.github.com>
2020-10-28 08:20:55 -0300

    Pilha

--- estruturas/stack.py
+++ estruturas/stack.py
@@ -3,11 +3,10 @@ class Elemento:
     self.conteudo = c
     self.proximo = p
 
-
 class Pilha:
   def __init__(self):
     self.topo = None 
-  
+
   def vazia(self): 
     if self.topo == None:
       print ("A pilha está vazia\n")
@@ -15,16 +14,16 @@ class Pilha:
       print ("A pilha não está vazia\n")
 
   def empilhar(self, novo_conteudo): 
-    novo_elemento = Elemento(novo_conteudo, self.topo)
-    self.topo = novo_elemento
-    print("Elemento {} foi inserido no topo da pilha\n".format(novo_elemento.conteudo))
+      novo_elemento = Elemento(novo_conteudo, self.topo)
+      self.topo = novo_elemento
+      print("Elemento {} foi inserido no topo da pilha\n".format(novo_elemento.conteudo))
 
   def desempilhar(self):
     if self.topo == None:
       print("A pilha está vazia\n")
     elemento_excluido = self.topo
     self.topo = self.topo.proximo
-    exclusao_feita = elemento_excluido.conteudo
+    exclusao_feita = elemento_excluido  # só removi o .conteudo
     del elemento_excluido
     print("Elemento ({}) foi excluído do topo da pilha\n".format(exclusao_feita))
 
@@ -43,5 +42,6 @@ class Pilha:
       print("A pilha está vazia\n")
     ver_conteudo = self.topo
     while ver_conteudo != None:
-      print(ver_conteudo.numero)
+      print(ver_conteudo.conteudo) # tava usando uma variavel numero que n tinha sido definida, simplesmente troquei por "conteudo" e lansei um \n pra ficar bonitinho
+      print("\n")
       ver_conteudo = ver_conteudo.proximo
