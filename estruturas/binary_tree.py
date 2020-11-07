f4aad98b6010ed487fb655fc32665c89d55405fd
Jacques Wolbeck <jacqueswolbeck@gmail.com>
2020-11-06 23:45:52 -0300

    Menu da Árvore

--- estruturas/binary_tree.py
+++ estruturas/binary_tree.py
@@ -1,5 +1,5 @@
 import pandas as pd
-csvData = pd.read_csv('https://raw.githubusercontent.com/audreyemmely/estrutura-de-dados/main/dados/USvideos.csv', encoding = "UTF-8")
+csvData = pd.read_csv('dados/USvideos.csv', encoding = "UTF-8")
 
 class Node:
 
@@ -64,20 +64,56 @@ class Node:
         print("\n")
         if self.right:
             self.right.showTree()
-            
+
+#-------------------MAIN-----------------------------
+def insertLoop(data, insertOption):
+    print("Quantos dados do arquivo você deseja inserir(0 a 100)?")
+    option = int(input("\nDigite aqui: "))
+    i = 1
+    while(i < option):
+        insertOption(data.iloc[i])
+        i+=1
+    print(option,"itens foram inseridos na lista.\n")
+
 
 def treeMenu(csvData):    
 
     data = csvData.sample(100)
     root = Node(data.iloc[0])
+
+    while(True):
+        print("---Menu da Árvore---")
+        print("1 - Inserir por Likes")
+        print("2 - Inserir por Views")
+        print("3 - Inserir por Título")
+        print("4 - Deletar por Likes ")
+        print("5 - Deletar por Views")
+        print("6 - Deletar por Título")
+        print("7 - Visualizar a Árvore")
+        print("8 - Sair")
+
+        option = int(input("\nDigite a sua escolha: "))
+        
+        if(option == 1):
+            insertLoop(data, lambda x: root.insertByLikes(x)) #(lambda argumento: expressão), é uma maneira de passar um função como argumento para outra função
+        elif(option == 2):
+            insertLoop(data, lambda x: root.insertByViews(x))
+        elif(option == 3):
+            insertLoop(data, lambda x: root.insertByTitle(x))
+        elif(option == 4):
+            print("Deletar por Likes")
+        elif(option == 5):
+            print("Deletar por Views")
+        elif(option == 6):
+            print("Deletar por Título")
+        elif(option == 7):
+            root.showTree()
+        elif(option == 8):
+            print("Saindo do Menu...\n")
+            break
+        else:
+            print("Tem 8 opções, escolha uma\n")
     
-    i=1
-    while(i < 10):
-                root.insertByViews(data.iloc[i]) 
-                i+=1
-
-
-    root.showTree()
 
 treeMenu(csvData)
 
