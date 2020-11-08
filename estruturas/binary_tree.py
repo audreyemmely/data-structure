099de23ac22c6e1ee3f60bd7a20fcbe8ff78759b
Jacques Wolbeck <jacqueswolbeck@gmail.com>
2020-11-08 16:18:50 -0300

    Árvore Binária Completa

--- estruturas/binary_tree.py
+++ estruturas/binary_tree.py
@@ -9,53 +9,22 @@ class Node:
         self.right = None
         self.data = data
 
-    def insertByLikes(self, data):
+    def insertNode(self, data, index):
 
-        if self.data[8]:
-            if data[8] < self.data[8]:
+        if self.data[index]:
+            if data[index] < self.data[index]:
                 if self.left == None:
                     self.left = Node(data)
                 else:
-                    self.left.insertByLikes(data)
-            elif data[8] > self.data[8]:
+                    self.left.insertNode(data, index)
+            elif data[index] > self.data[index]:
                 if self.right == None:
                     self.right = Node(data)
                 else:
-                    self.right.insertByLikes(data)
+                    self.right.insertNode(data, index)
         else:
             self.data = data
 
-    def insertByTitle(self, data):
-
-        if self.data[2]:
-            if data[2] < self.data[2]:
-                if self.left == None:
-                    self.left = Node(data)
-                else:
-                    self.left.insertByTitle(data)
-            elif data[2] > self.data[2]:
-                if self.right == None:
-                    self.right = Node(data)
-                else:
-                    self.right.insertByTitle(data)
-        else:
-            self.data = data
-
-    def insertByViews(self, data):
-
-        if self.data[7]:
-            if data[7] < self.data[7]:
-                if self.left == None:
-                    self.left = Node(data)
-                else:
-                    self.left.insertByViews(data)
-            elif data[7] > self.data[7]:
-                if self.right == None:
-                    self.right = Node(data)
-                else:
-                    self.right.insertByViews(data)
-        else:
-            self.data = data
 
     def showTree(self): #Em ordem
         if self.left:
@@ -64,14 +33,38 @@ class Node:
         print("\n")
         if self.right:
             self.right.showTree()
+    
+    
+    def findsuccessor(self, currentNode):
+        while currentNode.left != None:
+            currentNode = currentNode.left
+        return currentNode.data
+    
+    def deleteNode(self, currentNode, nodeToDelete, index):
+        if currentNode == None:
+            return currentNode
+        if(nodeToDelete < currentNode.data[index]):
+            currentNode.left = self.deleteNode(currentNode.left, nodeToDelete, index)
+        elif(nodeToDelete > currentNode.data[index]):
+            currentNode.right = self.deleteNode(currentNode.right, nodeToDelete, index)
+        else:
+            if currentNode.left == None:
+                currentNode = currentNode.right
+            elif currentNode.right == None:
+                currentNode = currentNode.left
+            else:
+               aux = self.findsuccessor(currentNode.right)
+               currentNode.data = aux
+               currentNode.right = self.deleteNode(currentNode.right, aux, index)
+        return currentNode
 
 #-------------------MAIN-----------------------------
-def insertLoop(data, insertOption):
+def insertLoop(root, data, index):
     print("Quantos dados do arquivo você deseja inserir(0 a 100)?")
     option = int(input("\nDigite aqui: "))
     i = 1
     while(i < option):
-        insertOption(data.iloc[i])
+        root.insertNode(data.iloc[i], index)
         i+=1
     print(option,"itens foram inseridos na lista.\n")
 
@@ -80,32 +73,45 @@ def treeMenu(csvData):
 
     data = csvData.sample(100)
     root = Node(data.iloc[0])
+    viewsIndex = 7
+    likesIndex = 8
+    commentsIndex = 10
+
 
     while(True):
         print("---Menu da Árvore---")
         print("1 - Inserir por Likes")
         print("2 - Inserir por Views")
-        print("3 - Inserir por Título")
+        print("3 - Inserir por Comentários")
         print("4 - Deletar por Likes ")
         print("5 - Deletar por Views")
-        print("6 - Deletar por Título")
+        print("6 - Deletar por Comentários")
         print("7 - Visualizar a Árvore")
         print("8 - Sair")
 
         option = int(input("\nDigite a sua escolha: "))
         
         if(option == 1):
-            insertLoop(data, lambda x: root.insertByLikes(x)) #(lambda argumento: expressão), é uma maneira de passar um função como argumento para outra função
+            insertLoop(root, data, likesIndex)
         elif(option == 2):
-            insertLoop(data, lambda x: root.insertByViews(x))
+            insertLoop(root, data, viewsIndex)
         elif(option == 3):
-            insertLoop(data, lambda x: root.insertByTitle(x))
+            insertLoop(root, data, commentsIndex)
         elif(option == 4):
             print("Deletar por Likes")
+            print("Digite o valor a ser deletado:")
+            nodeToDelete = int(input("\nDigite aqui: "))
+            root.deleteNode(root, nodeToDelete, likesIndex)
         elif(option == 5):
             print("Deletar por Views")
+            print("Digite o valor ser deletado:")
+            nodeToDelete = int(input("\nDigite aqui: "))
+            root.deleteNode(root, nodeToDelete, viewsIndex)
         elif(option == 6):
-            print("Deletar por Título")
+            print("Deletar por Quantidade de Comentários")
+            print("Digite o valor a ser deletado:")
+            nodeToDelete = int(input("\nDigite aqui: "))
+            root.deleteNode(root, nodeToDelete, commentsIndex)
         elif(option == 7):
             root.showTree()
         elif(option == 8):
@@ -117,4 +123,3 @@ def treeMenu(csvData):
 
 treeMenu(csvData)
 
-# Falta: Organizar menu, Funções delete
